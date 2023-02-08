5a29b43f138e49105b2a9cb676ef0b87003f0955
sdivakaran143 <sdivakaran683@gmail.com>
2023-02-09 00:31:35 +0530

    09-2-23

--- src/components/Maincontent.js
+++ src/components/Maincontent.js
@@ -2,15 +2,27 @@ import styles from '../App.module.css';
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import  'aos/dist/aos.css'
+import Divakaran from 'F:/Portfolio_Mine/profile/src/photo.png';
 function Maincontent(){
     useEffect(()=>{
         AOS.init({duration:2000});
     },[]);
     return(
+        <div>
         <div className={styles.content}>
-              <div className={styles.tittle} data-aos="fade-right">
-            <p>I'M Divakaran_Senthilkumar</p>
-            <span>Mern stack developer</span>
+            <div className={styles.contentintro} data-aos="fade-in">
+            <div className={styles.tittle} data-aos="fade-right">
+                <p>I'M Divakaran_Senthilkumar</p>
+                <span>Student at K.S.R. College Of engineering</span>
+                {/* <span>Mern stack developer</span> */}
+            </div>
+                <img className={styles.image} src={Divakaran}/>
+            </div>
+        </div>
+        <div>
+           {/* <h3>heading</h3> */}
+                <h3>MAinFest</h3>
+                <p>dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
         </div>
         </div>
     );
--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,6 +1,7 @@
 import styles from '../App.module.css';
 import React from 'react';
-import {BsFacebook,BsTwitter,BsGithub,BsInstagram,BsYoutube} from "react-icons/bs";
+import {BsFacebook,BsTwitter,BsGithub,BsInstagram,BsYoutube,BsLinkedin,BsWhatsapp} from "react-icons/bs";
+import {SiMinutemailer} from "react-icons/si";
 import { Link } from 'react-router-dom';
 export default function socialmedias(){
         return(
@@ -15,6 +16,11 @@ export default function socialmedias(){
                     <a title="GitHub" href="https://github.com/sdivakaran143"><BsGithub/></a>
                     <a title="Youtube" href="https://www.youtube.com/@saralstudio4449"><BsYoutube/></a>                   
                 </div>
+                <div className={styles.socialmedias}>
+                    <a title="Linked" href="https://www.linkedin.com/in/divakaran-senthilkumar-78779b248/"><BsLinkedin/></a>
+                     <a title="Whatsapp" href="https://api.whatsapp.com/send?phone=919500366762&text=Hai%20Diva%20!%20.%20.%20.%F0%9F%98%8D"><BsWhatsapp/></a>                   
+                </div>
+                <span className={styles.fixedmatter}><a href="mailto:divakarancse2020@ksrce.ac.in?bcc=sdivakaran683@gmail.com&subject=For%20Connection%20&body=Hai%20Iam%20%3CYour%20Name%3E!..."><SiMinutemailer/></a></span>
                 <Link to="/ContactMe"><button className={styles.conme}>Contact Me</button></Link>
             </div>
         </div>
