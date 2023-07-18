ace128df5cc639ef9a7df87d4c11b384ce8489b6
sdivakaran143 <sdivakaran683@gmail.com>
2023-07-19 00:29:50 +0530

    Portfolio Completed

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -30,9 +30,9 @@ function Navigationbar(props){
                 <li><Link className={styles.link} to="/About" >About</Link></li>
                 <li><Link className={styles.link} to="/Projects" >Projects</Link></li>
                 <li><Link className={styles.link} to="/ContactMe">Contact</Link></li>
-                {(pos)?<li className={styles.getintouch} data-aos="fade-in"><Link className={styles.link} onClick="#" to="/SocialMedia">Get In Touch</Link></li>:""}
+                {(pos)?<li className={styles.getintouch} data-aos="fade-in"><Link className={styles.link} to="/SocialMedia">Get In Touch</Link></li>:""}
             </ul>
-            <Link className={styles.logo} to="/ContactMe"><MdOutlineConnectWithoutContact className={styles.logo}/></Link>
+            <Link className={styles.logo} to="/SocialMedia"><MdOutlineConnectWithoutContact className={styles.logo}/></Link>
             <button className={styles.changeLogo} onClick={()=>{
                 changelogo();
                 props.changetheme();
