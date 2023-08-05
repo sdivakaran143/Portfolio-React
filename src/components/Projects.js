a3abd52eecf35ee9885cfbf2b8275ea2acd854d7
sdivakaran143 <sdivakaran683@gmail.com>
2023-08-05 23:12:06 +0530

    Optimized Final Build

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,8 +15,8 @@ export default function Projects(){
     const technologies=(tech)=>{
         return(
             <div className={styles.projecttechimagediv}>{
-                (tech).map((techImage,i) => (
-                    <img key={i} className={styles.projecttechimage} src={techImage} alt='projectimage' />
+                (tech).map((techImage) => (
+                    <img key={techImage.id} className={styles.projecttechimage} src={techImage} alt='projectimage' />
                 ))
           }
           </div>
@@ -60,10 +60,3 @@ export default function Projects(){
         </div>
     );
 }
-
-// <div className={styles.project_cards}>
-// <div className={styles.projectCenterContent}>
-//       {/* <img src="https://media.wired.com/photos/592699b8af95806129f4fff0/191:100/pass/GoogleHP-605827098.jpg"/> */}
-    // <img src="https://media.wired.com/photos/592699b8af95806129f4fff0/191:100/pass/GoogleHP-605827098.jpg"/>
-    // <div className={styles.projectdescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-// </div>
