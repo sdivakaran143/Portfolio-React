be48c53a07bfb30cb587239755db50c2a01730cf
sdivakaran143 <sdivakaran683@gmail.com>
2023-11-19 21:16:56 +0530

    upadated contactme and added certificate

--- src/components/Contactme.js
+++ src/components/Contactme.js
@@ -2,9 +2,10 @@ import React, { useRef,useState } from 'react';
 import  emailjs  from '@emailjs/browser';
 import styles from '../App.module.css';
 export default function Contactme() {
-    const[response,useresponse]=new useState("Click to Send Message");
+    const[response,useresponse]=new useState("Send Message");
     const form = useRef();
     const sendEmail = (e) => {
+        useresponse("Message Sending...")
         e.preventDefault();
         emailjs.sendForm('service_0kvlbhx', 'template_ko5wshq', form.current, 'wh1mZLy0HMSvFi7H4')
             .then((result) => {
@@ -17,32 +18,22 @@ export default function Contactme() {
 
     return (
         <div className={styles.contact_contents}>
-            <h2>Get In touch with Me</h2>
+            <h2>Text Me I'll Catch You..</h2>
             <div className={styles.main_content}>
                 <form ref={form} onSubmit={sendEmail} className={styles.form}>
-                    <fieldset>
-                        <legend>FirstName</legend>
-                        <input type="text" className={styles.halfinput} name="user_fname" placeholder="First Name" required/>
-                    </fieldset>
-                    <fieldset>
-                        <legend>LastName</legend>
-                        <input type="text" className={styles.halfinput} name="user_lname" placeholder="Last Name" required/>
-                    </fieldset>
-                    <fieldset>
-                        <legend>EmailId</legend>
-                        <input type="email" className={styles.fullinput} placeholder="Email id" name="user_email" required/>
-                    </fieldset>
-                    <fieldset>
-                        <legend>ContactNo</legend>
-                        <input type="number" className={styles.fullinput} name="user_phone" placeholder="Phone Number" required/>
-                    </fieldset>
-                    <fieldset>
-                        <legend>Message</legend>
+                        <label>FirstName</label>
+                        <input type="text" className={styles.halfinput} name="user_fname" placeholder="Divakaran" required/>
+                        <label>LastName</label>
+                        <input type="text" className={styles.halfinput} name="user_lname" placeholder="Senthilkumar" required/>
+                        <label>EmailId</label>
+                        <input type="email" className={styles.fullinput} placeholder="Email id" name="sdivakran683@gmail.com" required/>
+                        <label>ContactNo</label>
+                        <input type="number" className={styles.fullinput} name="user_phone" placeholder="9500366762" required/>
+                        <label>Message</label>
                         <textarea rows="4" name="message" cols="50" className={[styles.fullinput, styles.largebox].join(' ')} placeholder="Write a message"></textarea>
-                    </fieldset>
-                    <br />
-                    <button type="submit" className={styles.send_button} value="Send">Send Message</button>
-                    <span className={styles.responsemessage}>{response}</span>
+                        <br />
+                    <button type="submit" className={styles.send_button} value="Send">{response}</button>
+                    {/* <span className={styles.responsemessage}>{response}</span> */}
                 </form>
             </div>
 
